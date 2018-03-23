e5bffb932bbce5c6f28c861eb82b6814f3353279
KeoneAratani <35584306+KeoneAratani@users.noreply.github.com>
2018-03-22 18:21:49 -1000

    Some stuff

--- app/imports/ui/pages/ListContacts.jsx
+++ app/imports/ui/pages/ListContacts.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { Container, Header, Loader, Card } from 'semantic-ui-react';
 import { Contacts } from '/imports/api/contact/contact';
-import { Notes } from '/imports/api/note/note';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
 import Contact from '/imports/ui/components/Contact';
@@ -12,7 +11,7 @@ class ListContacts extends React.Component {
 
   /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
   render() {
-    return (this.props.ready) ? this.renderPage() : <Loader>Getting Data</Loader>;
+    return (this.props.ready) ? this.renderPage() : <Loader>LGetting Data</Loader>;
   }
 
   /** Render the page once subscriptions have been received. */
@@ -21,10 +20,7 @@ class ListContacts extends React.Component {
         <Container>
           <Header as="h2" textAlign="center" inverted>List Contacts</Header>
           <Card.Group>
-            {this.props.contacts.map((contact, index) =>
-                <Contact key={index}
-                  contact={contact}
-                  notes={this.props.notes.filter(note => (note.contactId === contact._id))}/> )}
+            {this.props.contacts.map((contact, index) => <Contact key={index} contact={contact} />)}
           </Card.Group>
         </Container>
     );
@@ -34,7 +30,6 @@ class ListContacts extends React.Component {
 /** Require an array of Stuff documents in the props. */
 ListContacts.propTypes = {
   contacts: PropTypes.array.isRequired,
-  notes: PropTypes.array.isRequired,
   ready: PropTypes.bool.isRequired,
 };
 
@@ -42,10 +37,8 @@ ListContacts.propTypes = {
 export default withTracker(() => {
   // Get access to Stuff documents.
   const subscription = Meteor.subscribe('Contacts');
-  const subscription2 = Meteor.subscribe('Notes');
   return {
     contacts: Contacts.find({}).fetch(),
-    notes: Notes.find({}).fetch(),
-    ready: (subscription.ready() && subscription2.ready()),
+    ready: subscription.ready(),
   };
 })(ListContacts);
